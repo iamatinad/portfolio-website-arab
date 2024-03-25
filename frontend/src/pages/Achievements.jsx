b8d325daa66da6f0d7042e1908030dea6d5956e5
DianaDzakah <dianadzakah1@gmail.com>
2024-03-25 16:30:04 +0000

    changes

--- frontend/src/pages/Achievements.jsx
+++ frontend/src/pages/Achievements.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import image1 from "../assets/Achievements background1.jpg"
 
 function Achievements() {
   return (
     <div>
       <Header />
-      <div>
-       <h2 class="text-center">ACHIEVEMENTS</h2>
-       <div class="grid-rows-3">
-        <div>
+      <div >
+        <div class="relative">
+       <img class="w-4/5 h-1/3" src={image1}/>
+       <div class="grid gap-4 grid-cols-3 grid-rows-3 mt-6" >
+        <div class="p-2.5">
           <h3>first</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
           </div>
-        <div>
+        <div class="p-2.5">
           <h3>second</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
           </div>
-        <div>
+        <div class="p-2.5">
           <h3>third</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
           </div>
+       </div>
        </div>
       </div>
       <Footer />
