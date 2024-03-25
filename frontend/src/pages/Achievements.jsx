efaf757e5e67c396c3841a74263afa282e731423
DianaDzakah <dianadzakah1@gmail.com>
2024-03-25 15:09:31 +0000

    changes done to achievements

--- frontend/src/pages/Achievements.jsx
+++ frontend/src/pages/Achievements.jsx
@@ -6,7 +6,23 @@ function Achievements() {
   return (
     <div>
       <Header />
-      <div>Achievements Page Goes Here</div>
+      <div>
+       <h2 class="text-center">ACHIEVEMENTS</h2>
+       <div class="grid-rows-3">
+        <div>
+          <h3>first</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
+          </div>
+        <div>
+          <h3>second</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
+          </div>
+        <div>
+          <h3>third</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
+          </div>
+       </div>
+      </div>
       <Footer />
     </div>
   );
