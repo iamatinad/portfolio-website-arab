7cc41bea703f1454322cec0c77d948d9b3e9e846
DianaDzakah <dianadzakah1@gmail.com>
2024-03-26 19:27:07 +0000

    changes

--- frontend/src/pages/Achievements.jsx
+++ frontend/src/pages/Achievements.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import image1 from "../assets/Achievements background1.jpg"
+import background1 from "../assets/background1.png"
+
+
+
 
 function Achievements() {
   return (
     <div>
       <Header />
-      <div >
-        <div class="relative">
-       <img class="w-4/5 h-1/3" src={image1}/>
-       <div class="grid gap-4 grid-cols-3 grid-rows-3 mt-6" >
-        <div class="p-2.5">
-          <h3>first</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
-          </div>
-        <div class="p-2.5">
-          <h3>second</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
-          </div>
-        <div class="p-2.5">
-          <h3>third</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis quibusdam, ab eum voluptas tempore quod. Beatae, possimus veritatis, iste quia ipsum quod numquam, laboriosam molestiae autem voluptatem quidem illo magnam voluptatum! Autem accusantium recusandae veritatis praesentium nemo iure assumenda atque rem delectus. Dolores repellendus consequatur earum qui at dolor optio quis aliquid unde ullam sequi, possimus ab eum, iure esse inventore accusamus nihil molestias. Unde soluta animi voluptatem pariatur, rem minus magnam debitis corrupti doloremque facilis totam hic repellendus dolorem optio? Debitis culpa modi dolorem illum, quas vero vitae assumenda reprehenderit officia quam? Reiciendis mollitia eaque dolorem accusantium possimus?</p>
-          </div>
-       </div>
-       </div>
+      <div style={{backgroundImage:`url(${background1})`}} class="h-screen p-10 w-full	bg-cover">
+      <h1 class="mb-8 font-bold text-8xl	" >ACHIEVEMENTS</h1>
+      <h2 class=" text-4xl mt-8 font-bold">We Made Our Mark</h2>
+      <div class=" grid	 grid-rows-3 gap-5  mix-blend-darken		mt-10	 bg-slate-200	">
+      <div class="">
+        <h3 class="font-medium		">Product Innovation Award</h3>
+        <p class="">Recognized for spearheading the development and launch <br/>of a groundbreaking product, driving increased sales and customer engagement.
+        </p>
+      </div>
+      <div>
+        <h3  class="font-medium		">Client Satisfaction Champion</h3>
+        <p> Received top honors for consistently exceeding client expectations, delivering <br/>exceptional service and results.</p>
+      </div>
+      </div>
       </div>
       <Footer />
     </div>
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
-function Projects() {
+const Projects = () => {
   return (
-     <div id= 'projects'className='max-w-[1040px] m-auto md:pl20 p-4 py-16'>
-    <h1>Projects</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis est rem debitis voluptas quaerat magni nesciunt quasi magnam architecto vel? Dicta nemo sunt nobis quis. Quam ducimus vitae veniam quo?</p>
-  </div>
+    <div>Projects</div>
   )
 }
 
