28270ac4f05d21b40e2f12775fefc5ea2a81a76b
DianaDzakah <dianadzakah1@gmail.com>
2024-03-27 14:01:44 +0000

    changes

--- frontend/src/pages/Experiences.jsx
+++ frontend/src/pages/Experiences.jsx
@@ -8,7 +8,19 @@ function Experiences() {
     <div>
       <Header />
       <div style={{backgroundImage:`url(${background2})`}} class="h-screen p-10 w-full	bg-cover">
-
+      <h1 class="mb-8 font-bold text-8xl	" >EXPERIENCES</h1>
+      <h2 class=" text-4xl mt-8 font-bold">HEROES ARE MADE</h2>
+      <div class=" grid	 grid-rows-3 gap-5  mix-blend-darken		mt-10	 bg-slate-200	">
+      <div class="">
+        <h3 class="font-bold">Agro-eCommerce Website</h3>
+        <p class="font-semibold	">Recognized for spearheading the development and launch <br/>of a groundbreaking product, driving increased sales and customer engagement.
+        </p>
+      </div>
+      <div>
+        <h3  class="font-bold		">Movies Streaming Website</h3>
+        <p class="font-semibold"> Received top honors for consistently exceeding client expectations, delivering <br/>exceptional service and results.</p>
+      </div>
+      </div>
       </div>
 
       
