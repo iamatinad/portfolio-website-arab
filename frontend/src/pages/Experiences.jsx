f2a1d16b4bedfb1f06bc07e65ae0149b80580221
DianaDzakah <dianadzakah1@gmail.com>
2024-03-27 17:47:08 +0000

    changes

--- frontend/src/pages/Experiences.jsx
+++ frontend/src/pages/Experiences.jsx
@@ -1,47 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import background2 from "../assets/background2.png"
+import background2 from "../assets/background2.png";
 import { apiGetAllExperiences } from "../services/experiences.services";
 
 function Experiences() {
-  const [experiences, setExperiences] = useState([])
+  const [experiences, setExperiences] = useState([]);
 
   useEffect(() => {
-    (async () => {
+    const fetchExperiences = async () => {
       try {
-        const results = await apiGetAllExperiences()
-        // console.log(results.data)
-        setExperiences(results.data)
-
+        const results = await apiGetAllExperiences();
+        setExperiences(results.data);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    })();
-  }, [])
+    };
+
+    fetchExperiences();
+  }, []);
 
   return (
     <div>
       <Header />
-      <div style={{backgroundImage:`url(${background2})`}} class="h-screen p-10 w-full	bg-cover">
-      <h1 class="mb-8 font-bold text-8xl	" >EXPERIENCES</h1>
-      <h2 class=" text-4xl mt-8 font-bold">HEROES ARE MADE</h2>
-      <div class=" grid	 grid-rows-3 gap-5  mix-blend-darken		mt-10	 bg-slate-200	">
-      {
-            experiences.map((experiences, index) => (
-              <div class="" key={index}>
-                <h3 class="font-bold">{experiences.role}</h3>
-                <p class="font-semibold">{experiences.company}</p>
-                <p>{experiences.duration}</p>
-                <p>{experiences.responsibilities}</p>
-                <img src={experiences.logo} alt="logo image" />
-              </div>
-            ))
-          }
+      <div
+        style={{ backgroundImage: `url(${background2})` }}
+        className="h-screen p-10 w-full bg-cover"
+      >
+        <h1 className="mb-8 font-bold text-8xl">EXPERIENCES</h1>
+        <h2 className="text-4xl mt-8 font-bold">HEROES ARE MADE</h2>
+        <div className="grid grid-rows-3 gap-5 mix-blend-darken mt-10 bg-slate-200">
+          {experiences.map((experience, index) => (
+            <div className="" key={index}>
+              <h3 className="font-bold">{experience.role}</h3>
+              <p className="font-semibold">{experience.company}</p>
+              <p>{experience.duration}</p>
+              <p>{experience.responsibilities}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      </div>
-
-      
       <Footer />
     </div>
   );
