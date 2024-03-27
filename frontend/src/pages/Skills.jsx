48fb78c12d9d26f5e07285416fd7d1d1dc6f1640
larissa-azuma <azumalinda29@gmal.com>
2024-03-27 04:27:11 +0000

    skills ongoing

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -6,10 +6,12 @@ function Skills() {
   return (
     <div>
       <Header />
-      <div>Skills Page Goes Here</div>
-      <Footer></Footer>
+      <div className="text-center text-3xl mt-5">SKILLS</div>
+      <div className="container mx-auto lg:px-5 lg:py-5">
+        <div className="text-center">Your skills content here</div>
+      </div>
+      <Footer />
     </div>
   );
 }
-
 export default Skills;
