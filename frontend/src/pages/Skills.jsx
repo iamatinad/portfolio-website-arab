7edd26d002c717223d3efb7c614e21cfe30724f2
larissa-azuma <azumalinda29@gmail.com>
2024-03-27 19:54:31 +0000

    skills--updated again

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import frontendImg from "../assets/images/frontend.png";
+import backendImg from "../assets/images/backend.png";
 
 function Skills() {
   return (
@@ -18,10 +20,11 @@ function Skills() {
           <div className="relative text-gray-700 antialiased text-sm font-semibold">
 
             {/*================vertical line running through the middle========  */}
-            <div className="hidden absolute w-l sm:block bg-indigo-300 h-full left-1/2 transform-translate-x-1/2"></div>
-
-            {/* ==================left card================== */}
-            <div className="mt-6 sm:mt-0 sm:mb-12">
+            <div className=" absolute w-2 sm:block bg-indigo-300 h-full left-1/2 transform-translate-x-1/2">
+            
+            </div>
+  {/* ==================left card================== */}
+  <div className="mt-6 sm:mt-0 sm:mb-12">
             <div className="flex items-center flex-col sm:flex-row">
 
             <div className="flex justify-start w-full mx-auto items-center"> 
@@ -35,14 +38,41 @@ function Skills() {
             </div>
 
             <div className="rounded-full bg-purple-500 border-white border-4 w-10 h-10 absolute left-1/2 transform-translate-x-1/2- translate-y-4 sm:translate-y-0 flex items-center justify-center">
+          <figure>
+            <img src= {frontendImg} alt=""/>
+          </figure>
+            </div>
+            </div>
             
             </div>
+
+
+    {/* =============rightcard========= */}
+    <div className="mt-6 sm:mt-0 sm:mb-12">
+            <div className="flex items-center flex-col sm:flex-row">
+
+            <div className="flex justify-end w-full mx-auto items-center"> 
+              <div className="w-full sm:w-1/2 sm:pl-8">
+                <div data-aos='fade-right' data-aos-duration='1200' className="bg-white p-4 rounded shadow group hover:bg-black cursor-pointer ease-in duration-150">
+                  <h3 className=" text-purple-700 font mb-3 group-hover:text-white group-hover:font-[600] text-xl">Backend Development</h3>
+
+                  <p className="text-[15px] text-blue-900 group-hover:text-white group-hover:font-[500] leading-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nam nisi excepturi culpa quidem est pariatur iste sunt a ipsum, minus maxime dolorum dolore at accusantium corrupti quisquam, distinctio iusto.</p>
+                </div>
+              </div>
             </div>
+
+            <div className="rounded-full bg-purple-500 border-white border-4 w-10 h-10 absolute right-1/2 transform-translate-x-1/2- translate-y-4 sm:translate-y-0 flex items-center justify-center">
+            <figure>
+            <img src= {backendImg} alt=""/>
+          </figure>
             </div>
+            </div> 
+            </div>
+            {/* ==================left card================== */}
+            
           </div>
           </div>
         </div>
-
       </div>     
       </div>
       <Footer />
